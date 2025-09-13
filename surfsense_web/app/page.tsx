e54b2df7b39120654b4a788494177f47d0a75133
Azekowka <gabjtov@gmail.com>
2025-09-13 19:03:22 +0500

    footer remove

--- surfsense_web/app/page.tsx
+++ surfsense_web/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { Footer } from "@/components/Footer";
 import { ModernHeroWithGradients } from "@/components/ModernHeroWithGradients";
 import { Navbar } from "@/components/Navbar";
 
@@ -9,7 +8,6 @@ export default function HomePage() {
 		<main className="min-h-screen bg-gradient-to-b from-gray-50 to-gray-100 text-gray-900 dark:from-black dark:to-gray-900 dark:text-white">
 			<Navbar />
 			<ModernHeroWithGradients />
-			<Footer />
 		</main>
 	);
 }
